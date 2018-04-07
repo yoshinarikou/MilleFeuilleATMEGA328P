3438da6ccbd066ca9612a865d867c044caf03add
ykou <yoshinari.kou@gmail.com>
2018-04-08 01:46:06 +0900

    Add files via upload

---
+++ mille-feuille_ATMEGASample/mille-feuile_DCMotor2ch/mille-feuile_DCMotor2ch.ino
@@ -0,0 +1,67 @@
+#include <inttypes.h>
+#include "MILLEIO.h"
+#include "MILLE.h"
+
+#define DCMOTOR2CH_ADDRESS 0x80000059
+
+sDevInfo infoDCMOTOR2CH;  //define structure for device information.
+uint8_t myWire[4];  //The wire for device.配線の情報を格納する変数
+
+mille myMille;//mille-feuille contorol instance.
+MILLEIO myDCMOTOR2CH(DCMOTOR2CH_ADDRESS, myWire,&infoDCMOTOR2CH,0);//class instance.ベースボードのコネクタ番号0につながっているデバイスのインスタンスを生成します
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  if(myMille.detectModule(&infoDCMOTOR2CH)){  //Find the device
+    exit(0);    //device conection error
+  }
+  pinMode(myWire[0],OUTPUT);
+  pinMode(myWire[1],OUTPUT);
+  pinMode(myWire[2],OUTPUT);
+  pinMode(myWire[3],OUTPUT);
+}
+
+void loop() {
+  myMille.connect(&infoDCMOTOR2CH);//wire from MCU to device. デバイスと配線を接続、この後は普通にArduinoのコードで走ります。
+  
+  Serial.println("connect\r\n");
+
+  //forward
+  Serial.println("forward\r\n");
+  digitalWrite(myWire[0],HIGH);
+  digitalWrite(myWire[1],LOW);
+  digitalWrite(myWire[2],HIGH);
+  digitalWrite(myWire[3],LOW);
+  delay(2000);
+  
+  //brake
+  Serial.println("brake\r\n");
+  digitalWrite(myWire[0],HIGH);
+  digitalWrite(myWire[1],HIGH);
+  digitalWrite(myWire[2],HIGH);
+  digitalWrite(myWire[3],HIGH);
+  delay(200);
+
+  //back
+  Serial.println("back\r\n");
+  digitalWrite(myWire[0],LOW);
+  digitalWrite(myWire[1],HIGH);
+  digitalWrite(myWire[2],LOW);
+  digitalWrite(myWire[3],HIGH);
+  delay(2000);
+
+  //no controll
+  Serial.println("non\r\n");
+  digitalWrite(myWire[0],HIGH);
+  digitalWrite(myWire[1],HIGH);
+  digitalWrite(myWire[2],HIGH);
+  digitalWrite(myWire[3],HIGH);
+  delay(200);
+  
+  Serial.println("disconnect\r\n");
+  
+  myMille.disconnect(&infoDCMOTOR2CH);//別のデバイスに接続する前に接続断をします。
+  delay(1000);
+    
+}
---
+++ mille-feuille_ATMEGASample/mille-feuile_detect/mille-feuile_detect.ino
@@ -0,0 +1,48 @@
+#include <inttypes.h>
+#include "MILLE.h"
+#include "MILLEIO.h"
+
+#define DEVICE_ADDRESS 0x80000000
+
+uint8_t myWire[4];  //The wire for device.配線の情報を格納する変数
+sDevInfo infoDevice;  //define structure for device information.
+
+mille myMille;//mille-feuille contorol instance.
+MILLEIO myDevice(DEVICE_ADDRESS,myWire,&infoDevice,0);//AD2CH class instance.ベースボードのコネクタ番号0につながっているデバイスのインスタンスを生成します
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("---Copy following text to your .txt file---");
+}
+
+void loop() {
+  unsigned int myAddr;
+  unsigned int data;
+  uint8_t connector;
+  for(myAddr=1;myAddr<=500;++myAddr){
+    infoDevice.address = infoDevice.address + 1;
+    if(!myMille.detectModule(&infoDevice)){  //Find the device
+      
+//      Serial.write("infoDevice.address = ");
+      data = infoDevice.address;
+//      Serial.print(data,HEX);
+//      Serial.write(" : myAddr = ");
+      Serial.print(myAddr);
+//      Serial.write(" I find!!\r\n");
+      connector = myAddr%4-1;
+      Serial.write("[");
+      myDevice.getMyWire(data,myWire,connector);
+      Serial.print(myWire[0]);
+      Serial.write(", ");
+      Serial.print(myWire[1]);
+      Serial.write(", ");
+      Serial.print(myWire[2]);
+      Serial.write(", ");
+      Serial.print(myWire[3]);
+      Serial.write("]");
+      Serial.write("\r\n");
+    }
+  }
+  exit(0);
+}
