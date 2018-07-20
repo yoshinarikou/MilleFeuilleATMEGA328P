e0078bb46de13e640f460b8f739c2c159e3efacf
yoshinarikou <yoshinari.kou@gmail.com>
2018-07-20 17:43:13 +0900

    change ordered detect devices

--- mille-feuille_ATMEGASample/mille-feuile_detect/mille-feuile_detect.ino
+++ mille-feuille_ATMEGASample/mille-feuile_detect/mille-feuile_detect.ino
@@ -3,6 +3,7 @@
 #include "MILLEIO.h"
 
 #define DEVICE_ADDRESS 0x80000000
+#define DEVICE_UART_ADDRESS 0xC0000000
 
 uint8_t myWire[4];  //The wire for device.配線の情報を格納する変数
 sDevInfo infoDevice;  //define structure for device information.
@@ -20,6 +21,10 @@ void loop() {
   unsigned int myAddr;
   unsigned int data;
   uint8_t connector;
+
+  
+  infoDevice.address = DEVICE_UART_ADDRESS;
+  
   for(myAddr=1;myAddr<=500;++myAddr){
     infoDevice.address = infoDevice.address + 1;
     if(!myMille.detectModule(&infoDevice)){  //Find the device
@@ -29,6 +34,33 @@ void loop() {
 //      Serial.print(data,HEX);
 //      Serial.write(" : myAddr = ");
       Serial.print(myAddr);
+//      Serial.write(" I find!!\r\n");
+      connector = myAddr%4-1;
+      Serial.write("[");
+      myDevice.getMyWire(data,myWire,connector);
+      Serial.print(myWire[0]);
+      Serial.write(", ");
+      Serial.print(myWire[1]);
+      Serial.write(", ");
+      Serial.print(myWire[2]);
+      Serial.write(", ");
+      Serial.print(myWire[3]);
+      Serial.write("]");
+      Serial.write("\r\n");
+    }
+  }
+
+  infoDevice.address = DEVICE_ADDRESS;
+  
+  for(myAddr=1;myAddr<=500;++myAddr){
+    infoDevice.address = infoDevice.address + 1;
+    if(!myMille.detectModule(&infoDevice)){  //Find the device
+      
+//      Serial.write("infoDevice.address = ");
+      data = infoDevice.address;
+//      Serial.print(data,HEX);
+//      Serial.write(" : myAddr = ");
+      Serial.print(DEVICE_UART_ADDRESS+myAddr);
 //      Serial.write(" I find!!\r\n");
       connector = myAddr%4-1;
       Serial.write("[");
