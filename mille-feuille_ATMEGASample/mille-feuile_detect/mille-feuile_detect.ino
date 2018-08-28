92b0f7410cfd060a1da60e7c0775d0f30bb68ecb
yoshinarikou <yoshinari.kou@gmail.com>
2018-08-28 11:14:58 +0900

    chaged detect file

--- mille-feuille_ATMEGASample/mille-feuile_detect/mille-feuile_detect.ino
+++ mille-feuille_ATMEGASample/mille-feuile_detect/mille-feuile_detect.ino
@@ -14,6 +14,7 @@ MILLEIO myDevice(DEVICE_ADDRESS,myWire,&infoDevice,0);// instance.ベースボ�
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.println("---Detect devices, it takes few minutes---");
   Serial.println("---Copy following text to your .txt file---");
 }
 
@@ -22,13 +23,46 @@ void loop() {
   unsigned int data;
   uint8_t connector;
 
-  
+  //infoDevice.address = DEVICE_ADDRESS;
   infoDevice.address = DEVICE_UART_ADDRESS;
+  for(myAddr=1;myAddr<=500;++myAddr){
+    infoDevice.address = infoDevice.address + 1;
+    if(!myMille.detectModule(&infoDevice)){  //Find the device
+      myWire[0]=0;
+      myWire[1]=0;
+      myWire[2]=0;
+      myWire[3]=0;
+//      Serial.write("infoDevice.address = ");
+      data = infoDevice.address;
+//      Serial.print(data,HEX);
+//      Serial.write(" : myAddr = ");
+      Serial.print(DEVICE_UART_ADDRESS+myAddr);
+//      Serial.write(" I find!!\r\n");
+      connector = myAddr%4-1;
+      Serial.write("[");
+      myDevice.getMyWire(data,myWire,connector);
+      Serial.print(myWire[0]);
+      Serial.write(", ");
+      Serial.print(myWire[1]);
+      Serial.write(", ");
+      Serial.print(myWire[2]);
+      Serial.write(", ");
+      Serial.print(myWire[3]);
+      Serial.write("]");
+      Serial.write("\r\n");
+    }
+  }
+    
+  //infoDevice.address = DEVICE_UART_ADDRESS;
+  infoDevice.address = DEVICE_ADDRESS;
   
   for(myAddr=1;myAddr<=500;++myAddr){
     infoDevice.address = infoDevice.address + 1;
     if(!myMille.detectModule(&infoDevice)){  //Find the device
-      
+      myWire[0]=0;
+      myWire[1]=0;
+      myWire[2]=0;
+      myWire[3]=0;
 //      Serial.write("infoDevice.address = ");
       data = infoDevice.address;
 //      Serial.print(data,HEX);
@@ -50,31 +84,5 @@ void loop() {
     }
   }
 
-  infoDevice.address = DEVICE_ADDRESS;
-  
-  for(myAddr=1;myAddr<=500;++myAddr){
-    infoDevice.address = infoDevice.address + 1;
-    if(!myMille.detectModule(&infoDevice)){  //Find the device
-      
-//      Serial.write("infoDevice.address = ");
-      data = infoDevice.address;
-//      Serial.print(data,HEX);
-//      Serial.write(" : myAddr = ");
-      Serial.print(DEVICE_UART_ADDRESS+myAddr);
-//      Serial.write(" I find!!\r\n");
-      connector = myAddr%4-1;
-      Serial.write("[");
-      myDevice.getMyWire(data,myWire,connector);
-      Serial.print(myWire[0]);
-      Serial.write(", ");
-      Serial.print(myWire[1]);
-      Serial.write(", ");
-      Serial.print(myWire[2]);
-      Serial.write(", ");
-      Serial.print(myWire[3]);
-      Serial.write("]");
-      Serial.write("\r\n");
-    }
-  }
   exit(0);
 }
